95936e5d986e073849f5e8e3fdf1b6eb491c2e42
rafael ibarra <rafaelibarra0795@gmail.com>
2026-02-17 15:32:11 -0300

    feat: scaffold sales module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { HealthModule } from './health/health.module';
 import { AuthModule } from './auth/auth.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ProductsModule } from './products/products.module';
+import { SalesModule } from './sales/sales.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ProductsModule } from './products/products.module';
     AuthModule,
     CategoriesModule,
     ProductsModule,
+    SalesModule,
   ],
 })
 export class AppModule {}
